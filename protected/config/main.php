132145ab383a3a3349ebadf8e861ce5d2f501c6b
craa <412280471@qq.com>
2014-08-12 22:46:09 +0800

    子模块测试

--- protected/config/main.php
+++ protected/config/main.php
@@ -28,6 +28,10 @@ return array(
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		*/
+		'weixin'=>array(
+            'class'=>'ext.weixin.WeixinModule',
+            'token'=>'chenhongwei',
+        ),
 	),
 
 	// application components
@@ -37,16 +41,16 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
+			'urlSuffix'=>'.html',
 			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+
 			),
 		),
-		*/
+		
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
